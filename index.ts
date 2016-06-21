d8d5cbb3e4e33a23cb9b0d006c744e39a49dc000
dario <darioyzf@gmail.com>
2016-06-21 10:18:07 +0200

    up

--- index.ts
+++ index.ts
@@ -2,26 +2,21 @@ export = function codeStream(data: {
     format?: string;
     width?: number;
     height?: number;
-    host?: string;
     hostname?: string;
-    port?: number;
+    http_port?: number;
     channel: string;
     img?: string;
+    rtmp_port?: number;
+    rtmp_hostname?: string;
+    http_hostname?: string;
 }) {
 
     if (!data) {
         throw "no data";
     } else if (!data.channel) {
         throw "no channel";
-    } else {
-        if (data.host) {
-            data.port = parseInt(data.host.split(":")[0]);
-            data.hostname = data.host.split(":")[1];
-        } else if (data.hostname && data.port) {
-            data.host = data.hostname + ":" + data.port;
-        } else {
-            throw "no host data";
-        }
+    } else if (!data.hostname && !data.rtmp_hostname && !data.http_hostname) {
+        throw "no host data";
     }
 
     let code;
@@ -32,6 +27,10 @@ export = function codeStream(data: {
 
         case "mobile":
 
+            if (!data.http_port) throw "no http port";
+
+            if (!data.http_hostname && data.hostname) data.http_hostname = data.hostname;
+
             if (!data.width || !data.height) {
 
                 data.width = 862;
@@ -41,21 +40,23 @@ export = function codeStream(data: {
                 }
             }
 
-
-            code = '<div style="width:' + data.width + ';height:' + data.height + ';text-align:center;"><a href="http://' + data.host + '/hls/' + data.channel + '.m3u8"><img src="' + data.img + '"></a></div>';
+            code = '<div style="width:' + data.width + ';height:' + data.height + ';text-align:center;"><a href="http://' + data.http_hostname + ":" + data.http_port + '/hls/' + data.channel + '.m3u8"><img src="' + data.img + '"></a></div>';
 
             break;
 
 
         default:
 
+            if (!data.rtmp_port) throw "no rtmp port";
+
+            if (!data.rtmp_hostname && data.hostname) data.rtmp_hostname = data.hostname;
+
             if (!data.width || !data.height) {
 
                 data.width = 862;
                 data.height = 465;
 
-                code = '<object width="' + data.width + '" height="' + data.height + '"><param name="movie" value="http://fpdownload.adobe.com/strobe/FlashMediaPlayback.swf"></param><param name="flashvars" value="src=rtmp%3A%2F%2F' + data.hostname + '%2Flive%2F' + data.channel + '&scaleMode=zoom"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://fpdownload.adobe.com/strobe/FlashMediaPlayback.swf" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="' + data.width + '" height="' + data.height + '" flashvars="src=rtmp%3A%2F%2F' + data.hostname + '%2Flive%2F' + data.channel + '&scaleMode=zoom"></embed></object>'
-
+                code = '<object width="' + data.width + '" height="' + data.height + '"> <param name="movie" value="http://matbury.com/strobe/StrobeMediaPlayback.swf"></param><param name="flashvars" value="src=rtmp%3A%2F%2F' + data.rtmp_hostname + '%3A' + data.rtmp_port + '%2Flive%2F' + data.channel + '&streamType=live&scaleMode=zoom"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><param name="wmode" value="direct"></param><embed src="http://matbury.com/strobe/StrobeMediaPlayback.swf" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" wmode="direct" width="' + data.width + '" height="' + data.height + '" flashvars="src=rtmp%3A%2F%2F' + data.rtmp_hostname + '%3A' + data.rtmp_port + '%2Flive%2F' + data.channel + '&streamType=live&scaleMode=zoom"></embed></object>'
 
             }
 
